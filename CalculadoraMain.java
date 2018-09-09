7e51ccee3cea7b795f4c2423f041b9021ce78ef6
Raul Pichardo <raul022107@gmail.com>
2018-09-09 17:05:38 -0400

    Launch App file

---
+++ CalculadoraMain.java
@@ -0,0 +1,10 @@
+package methosCalculadora;
+
+public class CalculadoraMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		calculadora calcu = new calculadora();
+	}
+
+}
